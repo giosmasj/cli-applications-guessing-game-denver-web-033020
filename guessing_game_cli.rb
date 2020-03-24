4dc79cd2b813493eb8577f75cec41b45dd3360e5
James Giosmas <giosmasj@gmail.com>
2020-03-24 02:55:49 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,4 +6,6 @@ def run_guessing_game
   if guess == number.to_s
     puts "You guessed the correct number!"
     elsif guess.downcase == "exit"
-    puts "Goodbye"
+    puts "Goodbye!"
+  else
+    puts "The computer guessed #{number}"
